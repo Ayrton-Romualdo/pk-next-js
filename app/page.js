9460c3c0a81952cf105bc27629ba4677776f9a03
Ayrton Romualdo <ayrtonromualdo@gmail.com>
2023-12-03 21:16:22 -0300

    Base Url

--- app/page.js
+++ app/page.js
@@ -1,8 +1,6 @@
 
-const baseUrl =
-  typeof process.env.NEXT_PUBLIC_BASE_URL_API.length !== undefined
-    ? 'http://localhost:3000'
-    : "http://129.159.56.94:3000";
+// const baseUrl = 'http://localhost:3000';
+const baseUrl = "http://129.159.56.94:3000";
 
 export default function Home() {
   return (
